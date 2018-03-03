bff554f4345856755eac5c389d7b71a1159d7bab
Deceree Quiamco <decereequiamco@gmail.com>
2018-03-04 02:33:27 +0800

    kinda fixeed

--- app/src/main/java/com/gte_iamax/android/iamax_gte/AddAccount.java
+++ app/src/main/java/com/gte_iamax/android/iamax_gte/AddAccount.java
@@ -2,12 +2,22 @@ package com.gte_iamax.android.iamax_gte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+
+import com.rilixtech.materialfancybutton.MaterialFancyButton;
 
 public class AddAccount extends AppCompatActivity {
+   private MaterialFancyButton clrBtn,addBtn;
+    private RadioGroup gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_account);
+
+        clrBtn = (MaterialFancyButton) findViewById(R.id.clear);
+        addBtn = (MaterialFancyButton) findViewById(R.id.add);
+        gender = (RadioGroup) findViewById(R.id.rgGender);
     }
+
 }
